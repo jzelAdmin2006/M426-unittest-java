8450344f33219fc8323236078c35214bc157d581
jo.zeller@protonmail.com <johannes_zeller@sluz.ch>
2023-05-05 17:20:08 +0200

    improvement according to given-when-then principle

--- src/test/java/tech/bison/trainee/calculator/AverageTest.java
+++ src/test/java/tech/bison/trainee/calculator/AverageTest.java
@@ -3,34 +3,37 @@ package tech.bison.trainee.calculator;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
+import org.assertj.core.api.ThrowableAssert.ThrowingCallable;
 import org.junit.jupiter.api.Test;
 
 public class AverageTest {
-  @Test
-  void oneAndFour_mean_isTwoPointFive() {
-    double[] numbers = new double[] { 1, 4 };
-    double expected = 2.5;
+	@Test
+	void oneAndFour_mean_isTwoPointFive() {
+		double[] numbers = new double[] { 1, 4 };
+		double expected = 2.5;
 
-    double actual = new Average(numbers).mean();
+		double actual = new Average(numbers).mean();
 
-    assertThat(actual).isEqualTo(expected);
-  }
+		assertThat(actual).isEqualTo(expected);
+	}
 
-  @Test
-  void fiveNumbers_mean_isCorrect() {
-    double[] numbers = new double[] { 1, 2, 3, 4, 5 };
-    double expected = 3;
+	@Test
+	void fiveNumbers_mean_isCorrect() {
+		double[] numbers = new double[] { 1, 2, 3, 4, 5 };
+		double expected = 3;
 
-    double actual = new Average(numbers).mean();
+		double actual = new Average(numbers).mean();
 
-    assertThat(actual).isEqualTo(expected);
-  }
+		assertThat(actual).isEqualTo(expected);
+	}
 
-  @Test
-  void noNumbers_mean_isNotSupported() {
-    double[] numbers = new double[] {};
+	@Test
+	void noNumbers_mean_isNotSupported() {
+		double[] numbers = new double[] {};
 
-    assertThatThrownBy(() -> new Average(numbers).mean()).isInstanceOf(IllegalStateException.class)
-        .hasMessageContaining("undefined");
-  }
+		ThrowingCallable shouldRaiseThrowable = () -> new Average(numbers).mean();
+
+		assertThatThrownBy(shouldRaiseThrowable).isInstanceOf(IllegalStateException.class)
+				.hasMessageContaining("undefined");
+	}
 }
